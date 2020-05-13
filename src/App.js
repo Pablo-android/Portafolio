784f0d6c38acfb488c1eed96604e0972d6bf93cf
Fenix <galdorgem@hotmail.com>
2020-05-12 22:42:17 -0500

    A qui ya llevamos la funcion de los aplicativos

--- src/App.js
+++ src/App.js
@@ -1,39 +1,69 @@
 import React from 'react';
-import './styler.css';
-import { Route } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import Logo from './img/Dra.png';
+import './index.css';
+
+/* import { Route } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'; */
 
 
 function App() {
   return (
-      <div>
-        <header>
-          <nav class="nav-main">
-            <a href="/" > <img src={Logo} class="logo"/> </a>
-            <ul class="nav-menu">
-              <li>
-                <a href="h">PABLO</a>
-              </li>
-            </ul>
+    <div class="pantalla">
+      <div class="container">
+        <nav class="nav-main">
+           <a href="/">  <img src={ Logo } class="logo"></img> </a>
+          <ul class="nav-menu">
+            <li>
+              <a href="/">Pablo</a>
+            </li>
+          </ul>
+          <ul class="nav-menu-rigth">
+            <li >
+              <a href="/">F</a>
+            </li>
+            <li >
+              <a href="/">F</a>
+            </li>
+          </ul>
+        </nav>
+        <hr></hr>
+      </div>
 
-            <ul classs="nav menu-right">
-              <li>
-                <a href="h">PABLO</a>
-              </li>
-              <li>
-                <a href="h">PABLO</a>
-              </li>
-              
-            </ul>
+        {/* SHOWCASE */}
+      <header class="showcase">
+        <h2>Hola</h2>
+        <p>Lorem20</p>
+        <a href="#" class="btn">Leer mas</a>
 
-          </nav>
-            <hr></hr>
-        </header>
+
+      </header>
+
+      {/* LOS APLICATIVOS */}
+      <div class="aplica">
+        <div>
+          <img src="img/compu.jpeg" alt="New 1"></img>
+          <h3>Reprehenderit ullamco ea do quis eiusmod esse irure.</h3>
+          <p>Magna aliqua est eiusmod commodo nulla laboris culpa nulla voluptate voluptate occaecat commodo.</p>
+          {/* A qui es la base de el fichero */}
+          <a href="#">Loren More>><i class=""></i></a>
+
+          <img src="img/compu1.jpeg" alt="New 2"></img>
+          <h3>Reprehenderit ullamco ea do quis eiusmod esse irure.</h3>
+          <p>Magna aliqua est eiusmod commodo nulla laboris culpa nulla voluptate voluptate occaecat commodo.</p>
+          {/* A qui es la base de el fichero */}
+          <a href="#">Loren More>><i class=""></i></a>
+
+          <img src="img/compu2.jpeg" alt="New 3"></img>
+          <h3>Reprehenderit ullamco ea do quis eiusmod esse irure.</h3>
+          <p>Magna aliqua est eiusmod commodo nulla laboris culpa nulla voluptate voluptate occaecat commodo.</p>
+          {/* A qui es la base de el fichero */}
+          <a href="#">Loren More>><i class=""></i></a>
+
+        </div>
 
 
       </div>
-
+    </div>  
   );
 }
 
---
+++ src/Nav_bar/index.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import Logo from './../img/Dra.png';
+import './index.css';
+
+
+class Header extends Component {
+    render () {
+        return (
+            
+
+          
+        );
+    }
+}
+
+export default Header;
