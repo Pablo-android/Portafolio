4982bfbd85f5e2531c9a6c100b5ec0681f82b8e7
Fenix <galdorgem@hotmail.com>
2020-05-16 22:18:59 -0500

    Ya quedo la paguina, falta los navegadores de proyecto y skills

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       {/* Navegación */}
       <div class="container">
         <nav class="nav-main">
-           <a href="/">  <img src={ Logo } class="logo"> </img> </a>
+           <a href="/"><img src={ Logo } class="logo" alt="Nav"/></a>
           <ul class="nav-menu">
             <li>
               <a href="/">Pablo</a>
@@ -34,35 +34,146 @@ function App() {
       <header class="showcase">
         <h2>Hola</h2>
         <p>Irure enim exercitation ea ullamco amet sit quis occaecat ullamco culpa nisi tempor nulla.</p>
-        <a href="#" class="btn">Leer mas</a>
+        <a href="/" class="btn">Leer mas</a>
       </header>
 
-      {/* LOS APLICATIVOS */}
+      {/* LOS SKILLS */}
       <div class="aplica">
         <div>
-          <img src="img/compu.jpeg" alt="New 1"></img>
-          <h3>Reprehenderit ullamco ea do quis eiusmod esse irure.</h3>
+          <h1 class="titulo">Aplicativos que Manejo</h1>
+          <br/>
+          <img src="img/html.png" alt="HTML"></img>
+          <h3>HTML 5</h3>
           <p>Magna aliqua est eiusmod commodo nulla laboris culpa nulla voluptate voluptate occaecat commodo.</p>
           {/* A qui es la base de el fichero */}
-          <a href="#">Loren More>><i class=""></i></a>
+          <a href="/">Loren More<i class="fas fa-angle-double-right"></i></a>
 
-          <img src="img/compu1.jpeg" alt="New 2"></img>
-          <h3>Reprehenderit ullamco ea do quis eiusmod esse irure.</h3>
+          <img src="img/css.jfif" alt="CSS"></img>
+          <h3>CSS</h3>
           <p>Magna aliqua est eiusmod commodo nulla laboris culpa nulla voluptate voluptate occaecat commodo.</p>
           {/* A qui es la base de el fichero */}
-          <a href="#">Loren More>><i class=""></i></a>
+          <a href="/">Loren More<i class="fas fa-angle-double-right"></i></a>
 
-          <img src="img/compu2.jpeg" alt="New 3"></img>
-          <h3>Reprehenderit ullamco ea do quis eiusmod esse irure.</h3>
+          <img src="img/bootstrap.jfif" alt="Bootstrap"></img>
+          <h3>Bootstrap 4</h3>
           <p>Magna aliqua est eiusmod commodo nulla laboris culpa nulla voluptate voluptate occaecat commodo.</p>
           {/* A qui es la base de el fichero */}
-          <a href="#">Loren More>><i class=""></i></a>
-
+          <a href="/" class="btn">Loren More<i class="fas fa-angle-double-right"></i></a>
         </div>
+      </div>
 
+      {/* Proposito */}      
+      <section class="baner">
+        <div class="content">
+          <h2>Al generar un proyecto</h2>
+          <p>Officia pariatur aliqua tempor mollit elit nulla id incididunt sunt excepteur et aliquip officia sit.</p>
+          <a href="/" class="btn">Lorem More<i class="fas fa-angle-double ride"></i></a>
+        </div>
+      </section>
 
+      {/* PROYECTOS */}
+      <div class="aplica">
+        <div>
+          <h1 class="titulo">Lo proyectos realizados</h1>
+          <br/>
+          <img src="img/html.png" alt="HTML"></img>
+          <h3>HTML 5</h3>
+          <p>Magna aliqua est eiusmod commodo nulla laboris culpa nulla voluptate voluptate occaecat commodo.</p>
+          {/* A qui es la base de el fichero */}
+          <a href="/">Loren More<i class="fas fa-angle-double-right"></i></a>
+
+          <img src="img/css.jfif" alt="CSS"></img>
+          <h3>CSS</h3>
+          <p>Magna aliqua est eiusmod commodo nulla laboris culpa nulla voluptate voluptate occaecat commodo.</p>
+          {/* A qui es la base de el fichero */}
+          <a href="/">Loren More<i class="fas fa-angle-double-right"></i></a>
+
+          <img src="img/bootstrap.jfif" alt="Bootstrap"></img>
+          <h3>Bootstrap 4</h3>
+          <p>Magna aliqua est eiusmod commodo nulla laboris culpa nulla voluptate voluptate occaecat commodo.</p>
+          {/* A qui es la base de el fichero */}
+          <a href="/" class="btn">Loren More<i class="fas fa-angle-double-right"></i></a>
+        </div>
+      </div>
+
+      {/* Agradecimientos */}
+      <section class="proyecto">
+        <div class="proyecto-one">
+          <h2>Lorem</h2>
+          <p>Est enim Lorem quis incididunt labore ex excepteur.</p>
+          <a href="/" class="btn">Leer mas</a>
+        </div>
+      </section>
+
+      {/* SOCIALES */}
+        <section class="social">
+          <p class="nav-menu">Redes Sociales</p>
+            <div class="link">
+                <a href="/">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="/">
+                  <i class="fab-fa-thitter-fa"></i>
+                </a>
+            </div>
+        </section>
+      <div>
+          <div class="footer-links">
+            <div class="footer-container">
+              <ul>
+                <li>
+                  <a href="/">
+                    <h3>Title One</h3>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">Blockchain></a>
+                </li>
+                <li>
+                  <a href="/">Blockchain></a>
+                </li>
+                <li>
+                  <a href="/">Blockchain></a>
+                </li>
+                <li>
+                  <a href="/">Blockchain></a>
+                </li>
+                <li>
+                  <a href="/">Blockchain></a>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <a href="/">
+                    <h3>Title One</h3>
+                  </a>
+                </li>
+                <li>
+                  <a href="/">Blockchain></a>
+                </li>
+                <li>
+                  <a href="/">Blockchain></a>
+                </li>
+                <li>
+                  <a href="/">Blockchain></a>
+                </li>
+                <li>
+                  <a href="/">Blockchain></a>
+                </li>
+                <li>
+                  <a href="/">Blockchain></a>
+                </li>
+              </ul>
+            </div>
+          
+          </div>
+
+          <footer class="footer">
+            <h3>TECH NEWS COPYRGT</h3>
+          </footer>
       </div>
     </div>  
+
   );
 }
 
